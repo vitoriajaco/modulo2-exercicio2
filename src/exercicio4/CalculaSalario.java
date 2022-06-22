b060732992bb95636674075741d557e3c05bee49
vitoriajaco <vitoriajaco@gmail.com>
2022-06-22 15:30:53 -0300

    Exercicio 5 - Corrigido

--- src/exercicio4/CalculaSalario.java
+++ src/exercicio4/CalculaSalario.java
@@ -8,15 +8,20 @@ public class CalculaSalario {
         System.out.println("Informe seu salario: ");
         double salario = input.nextDouble();
 
-        System.out.println("Informe quantas horas você trabalha por dia: ");
+        System.out.println("Informe quantas horas você trabalhou por dia: ");
         double horas = input.nextDouble();
 
-        System.out.println("Informe quantos dias você trabalha no mês: ");
+        System.out.println("Informe quantos dias você trabalhou no mês: ");
         int dias = input.nextInt();
 
         input.close();
 
-        double HoraTrabalhada = (salario / dias) / horas;
+        double HoraTrabalhada = ((salario / dias) / horas);
+        double SalarioAReceber = (HoraTrabalhada * horas * dias);
+
         System.out.printf("Você recebe por hora o valor de R$ %.2f ", HoraTrabalhada);
+        System.out.printf("Você receberá o salário de R$ %.2f ", SalarioAReceber);
+
+
     }
 }
