bf87ea913c6d794f4a7b1a2bf23dda255bbc8945
vitoriajaco <vitoriajaco@gmail.com>
2022-06-22 15:54:46 -0300

    Exercicio 6

--- src/exercicio6/NumeroAnteriorEPosterior.java
+++ src/exercicio6/NumeroAnteriorEPosterior.java
@@ -1,5 +1,18 @@
 package exercicio6;
 
+import java.util.Scanner;
+
 public class NumeroAnteriorEPosterior {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Digite o número desejado");
+        int numero = input.nextInt();
+
+        input.close();
+
+        int NumeroAnterior = (numero) - 1;
+        int NumeroPosterior = (numero) + 1;
+        System.out.println("Você digitou o número " + numero + " seu número anterior é " + NumeroAnterior + ", e seu número posterior é " + NumeroPosterior );
+    }
 
 }
