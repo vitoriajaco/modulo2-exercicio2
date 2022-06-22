9a14bb8324e16a858be808f66cb7635f102eaada
vitoriajaco <97560091+vitoriajaco@users.noreply.github.com>
2022-06-22 12:31:21 -0300

    exercicio2-MediaAritmetica

--- src/exercicio2/MediaAritmetica.java
+++ src/exercicio2/MediaAritmetica.java
@@ -1,2 +1,28 @@
-package exercicio2;public class MediaAritmetica {
+package exercicio2;
+
+import java.util.Scanner;
+
+public class MediaAritmetica {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Digite a primeira nota: ");
+        double primeiranota = input.nextDouble();
+
+        System.out.print("Digite a segunda nota: ");
+        double segundanota = input.nextDouble();
+
+        System.out.println("Digite a terceira nota: ");
+        double terceiranota = input.nextDouble();
+
+        System.out.println("Digite a quarta nota: ");
+        double quartanota = input.nextDouble();
+
+        input.close();
+
+        int bimestres = 4;
+
+        double MediadeNotas = (primeiranota + segundanota + terceiranota + quartanota) / bimestres;
+        System.out.println("Sua média é: " + MediadeNotas);
+    }
 }
